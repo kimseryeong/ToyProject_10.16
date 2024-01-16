36e376407e626c92905bc17b1db68fdddb87ec3d
kimseryeong <kxxoo322@gmail.com>
2024-01-16 19:37:50 +0900

    경로 재설정 & 로그인

---
+++ toy/src/main/java/com/toy/controller/member/LoginController.java
@@ -0,0 +1,14 @@
+package com.toy.controller.member;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+
+	@RequestMapping("login.do")
+	public String login() throws Exception{
+		
+		return "views/login";
+	}
+}
